2ce54c0b82b82b6141e5552451fe95ed5a1e5327
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2017-06-02 10:56:18 +0200

    9069 - Rajouter le lien vers la tache à partir du risque (rajout de tache)

--- modules/corrective_task/view/form.view.php
+++ modules/corrective_task/view/form.view.php
@@ -4,7 +4,7 @@
  *
  * @author Jimmy Latour <jimmy@evarisk.com>
  * @since 0.1
- * @version 6.2.9.0
+ * @version 6.2.10.0
  * @copyright 2015-2017 Evarisk
  * @package corrective-task
  * @subpackage view
@@ -15,5 +15,7 @@ namespace digi;
 if ( ! defined( 'ABSPATH' ) ) { exit; } ?>
 
 <?php do_shortcode( '[task id="' . $task->id . '"]' ); ?>
+
+<a href="<?php echo esc_attr( admin_url( 'admin.php?page=wpeomtm-dashboard&term=' . $task->id ) ); ?>"><?php esc_html_e( 'Aller vers la tâche', 'digirisk' ); ?></a>
 
 <div class="button green margin uppercase strong float right"><span>OK</span></div>
